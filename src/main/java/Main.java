476bd294a37019a26ac07750307eba72fdb9f6ba
Nutrymaco <efim7299@icloud.com>
2020-11-14 11:34:46 +0300

    add time check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,13 @@ import com.nutrymaco.parser.job.GJParser;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        long start = System.currentTimeMillis();
+
         while (true){
             GJParser parser = new GJParser(24 * 3600);
             parser.start();
+
+            Thread.sleep(24*3600*1000 - start);
         }
     }
 }
